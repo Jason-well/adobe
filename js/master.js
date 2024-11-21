41720e027ba65c570c9339831c97897a0e211f23
Jason-well <a25513232@gmail.com>
2024-11-21 21:56:18 +0800

    update js(11/21)

---
+++ js/master.js
@@ -0,0 +1,15 @@
+console.log($(window).width());
+
+// let isMobile ;
+
+// if($(window).width() > 480) {
+//     isMobile = false;
+    
+// }else { isMobile = true;
+
+// }
+
+const isMobile  = $(window).width() <= 480
+
+
+console.log(isMobile);
