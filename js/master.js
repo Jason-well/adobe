199d576cf62e00be27b70c9382765edfd6634c68
Jason-well <a25513232@gmail.com>
2024-11-28 20:50:42 +0800

    完成(11/28)

--- js/master.js
+++ js/master.js
@@ -10,6 +10,64 @@ console.log($(window).width());
 // }
 
 const isMobile  = $(window).width() <= 480
+const $grid = $('#Grid');
 
 
-console.log(isMobile);
+
+// console.log(isMobile);
+// console.log($grid);
+
+
+
+function setGrid() {
+    if(isMobile === true) {
+    $grid.addClass('grid-2');
+    return; //(後面都不會執行了)
+}
+    $grid.addClass('grid-3');
+}
+// 把函式封裝
+
+// 要呼叫 function 才會用(就可以共用)
+
+// === 比較嚴景的判斷法
+
+// console.log( '1 == 1:', 1 == 1);
+// console.log( '1 === 1:', 1 === 1);
+// console.log( '1 == "1:"', 1 == 1);
+// console.log( '1 === "1:"', 1 === '1');
+
+function setScroll () {
+    if(!isMobile)return;
+
+    const $nav = $('#Nav')
+
+        $(window).scroll(function(){
+    if($(this).scrollTop()>0) {
+        $nav.addClass('nav-active');
+        return;
+    }
+    $nav.removeClass('nav-active');
+}
+);}
+
+
+
+
+
+function setInit() {
+    setGrid();
+}
+
+function setEvent(){
+    setScroll();
+}
+
+setEvent();
+setInit();
+
+
+
+
+
+
